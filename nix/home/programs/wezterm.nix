31705aa69753964774d5ab832aefcd1ea07473f6
mkiin <mkiin@users.noreply.github.com>
2026-06-25 01:43:21 +0900

    fix: NVIDIA EGL vendor を wrapper に追加

--- nix/home/programs/wezterm.nix
+++ nix/home/programs/wezterm.nix
@@ -3,6 +3,7 @@
 let
   wezterm-wrapped = pkgs.writeShellScriptBin "wezterm" ''
     export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
+    export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
     export WGPU_BACKEND=vulkan
     exec ${pkgs.wezterm}/bin/wezterm "$@"
   '';
