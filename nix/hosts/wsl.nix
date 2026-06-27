10775988ac7ef41932fc285e8291c16b00a90a18
mkiin <mkiin@users.noreply.github.com>
2026-06-27 14:11:00 +0900

    fix wsl shell startup directory

--- nix/hosts/wsl.nix
+++ nix/hosts/wsl.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   home.packages = with pkgs; [
@@ -6,7 +6,15 @@
     zip
   ];
 
-  programs.zsh.shellAliases = {
-    open = "explorer.exe .";
+  programs.zsh = {
+    shellAliases = {
+      open = "explorer.exe .";
+    };
+
+    initContent = lib.mkAfter ''
+      if [[ -o interactive && -n "''${WSL_DISTRO_NAME:-}" && "$PWD" == /mnt/[a-zA-Z]/* ]]; then
+        cd "$HOME"
+      fi
+    '';
   };
 }
