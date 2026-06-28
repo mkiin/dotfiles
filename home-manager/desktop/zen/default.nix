563276386f55189e7f558c73086fe1f6b779e5de
mkiin <mkiin@users.noreply.github.com>
2026-06-29 08:04:28 +0900

    feat(zen): add pref/language/font settings

--- home-manager/desktop/zen/default.nix
+++ home-manager/desktop/zen/default.nix
@@ -7,6 +7,36 @@
     profiles.default = {
       settings = {
         "browser.tabs.warnOnClose" = false;
+
+        # 定番UX
+        "zen.welcome-screen.seen" = true;
+        "browser.aboutConfig.showWarning" = false;
+        "zen.workspaces.continue-where-left-off" = true;
+
+        # コンパクトモード + フロートURLバー
+        "zen.view.compact.hide-tabbar" = true;
+        "zen.urlbar.behavior" = "float";
+
+        # プライバシー / テレメトリ off
+        "datareporting.healthreport.uploadEnabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+
+        # タブ運用
+        "zen.tabs.dim-pending" = true;
+        "zen.ctrlTab.show-pending-tabs" = true;
+
+        # 言語（Accept-Language ヘッダのみ）
+        "intl.accept_languages" = "ja,en-US,en";
+
+        # フォント（ja = 日本語 / x-western = 欧文）
+        "font.name.serif.ja" = "Noto Serif CJK JP";
+        "font.name.sans-serif.ja" = "Noto Sans CJK JP";
+        "font.name.monospace.ja" = "UDEV Gothic";
+        "font.default.ja" = "sans-serif";
+        "font.name.serif.x-western" = "Noto Serif";
+        "font.name.sans-serif.x-western" = "Inter";
+        "font.name.monospace.x-western" = "JetBrainsMono Nerd Font";
       };
       bookmarks = import ./bookmarks.nix;
     };
