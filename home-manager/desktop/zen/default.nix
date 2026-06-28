e351e6e6b6fb003ec8b76f8d4f4d59cfa12ec8c8
mkiin <mkiin@users.noreply.github.com>
2026-06-29 08:07:33 +0900

    feat(zen): declare Personal/Dev spaces and UI mods

--- home-manager/desktop/zen/default.nix
+++ home-manager/desktop/zen/default.nix
@@ -38,6 +38,28 @@
         "font.name.sans-serif.x-western" = "Inter";
         "font.name.monospace.x-western" = "JetBrainsMono Nerd Font";
       };
+
+      spacesForce = true;
+      spaces = {
+        "Personal" = {
+          id = "5d4c9e3d-e72a-4bd3-9c28-a0890768ded1";
+          position = 1000;
+          icon = "🏠";
+        };
+        "Dev" = {
+          id = "e3e428d8-c7e8-4d3b-9e29-d10aa3f80cae";
+          position = 2000;
+          icon = "💻";
+        };
+      };
+
+      mods = [
+        "e122b5d9-d385-4bf8-9971-e137809097d0" # No Top Sites
+        "253a3a74-0cc4-47b7-8b82-996a64f030d5" # Floating History
+        "803c7895-b39b-458e-84f8-a521f4d7a064" # Hide Inactive Workspaces
+        "906c6915-5677-48ff-9bfc-096a02a72379" # Floating Status Bar
+      ];
+
       bookmarks = import ./bookmarks.nix;
     };
   };
