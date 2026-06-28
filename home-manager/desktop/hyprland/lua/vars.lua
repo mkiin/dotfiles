18141f2da254257fd9e84e9af0b597f9e55082c6
mkiin <mkiin@users.noreply.github.com>
2026-06-29 05:55:19 +0900

    fix(desktop): browser=zen-beta (flake binary name), mimeApps zen-beta.desktop

--- home-manager/desktop/hyprland/lua/vars.lua
+++ home-manager/desktop/hyprland/lua/vars.lua
@@ -1,6 +1,6 @@
 local M = {}
 M.terminal    = "wezterm"
 M.fileManager = "wezterm start -- yazi"
-M.browser     = "zen-browser"
+M.browser     = "zen-beta"
 M.mainMod     = "SUPER"
 return M
--- sddm/themes/astronaut/README.md
+++
@@ -1,3 +0,0 @@
-# Preview
-
-![Preview](./Previews/image.png)
