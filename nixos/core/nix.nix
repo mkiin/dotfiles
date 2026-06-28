236f7d538330d1f6a4a36e981eb83967641c52fd
mkiin <mkiin@users.noreply.github.com>
2026-06-29 03:46:23 +0900

    feat(nixos): add hyprland cachix substituter

--- nixos/core/nix.nix
+++ nixos/core/nix.nix
@@ -1,6 +1,10 @@
 { ... }:
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.substituters = [ "https://hyprland.cachix.org" ];
+  nix.settings.trusted-public-keys = [
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  ];
   nix.gc = {
     automatic = true;
     options = "--delete-older-than 14d";
