8a44a5263e1622560e37220be3cab0dec86b14f7
mkiin <mkiin@users.noreply.github.com>
2026-07-01 00:53:43 +0900

    feat(nix): trusted-users に root と mkiin を追加

--- nixos/core/nix/default.nix
+++ nixos/core/nix/default.nix
@@ -3,6 +3,10 @@ _: {
     "nix-command"
     "flakes"
   ];
+  nix.settings.trusted-users = [
+    "root"
+    "mkiin"
+  ];
   nix.settings.substituters = [ "https://hyprland.cachix.org" ];
   nix.settings.trusted-public-keys = [
     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
