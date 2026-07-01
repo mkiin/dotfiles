9d98cfdec1a0e9fffd869e57e06579cd6b43602c
mkiin <mkiin@users.noreply.github.com>
2026-07-01 22:40:29 +0900

    chore(nix): cachix を flake に一本化し accept-flake-config で警告解消

--- nixos/core/nix/default.nix
+++ nixos/core/nix/default.nix
@@ -7,10 +7,7 @@ _: {
     "root"
     "mkiin"
   ];
-  nix.settings.substituters = [ "https://hyprland.cachix.org" ];
-  nix.settings.trusted-public-keys = [
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-  ];
+  nix.settings.accept-flake-config = true;
   nix.gc = {
     automatic = true;
     options = "--delete-older-than 14d";
