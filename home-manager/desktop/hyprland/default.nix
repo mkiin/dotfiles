857ef83f9c8563e7c3d4e60c79d0b453d76e2483
mkiin <mkiin@users.noreply.github.com>
2026-07-11 02:58:15 +0900

    feat(hyprlock): 他 dotfiles 由来のロック画面を実験用に 4 版退避
    cybersnake / blacknode / berthosefin / hyprcraft を hyprlock.<name>.conf として保持し default.nix で配置。

--- home-manager/desktop/hyprland/default.nix
+++ home-manager/desktop/hyprland/default.nix
@@ -28,6 +28,10 @@
     "hypr/rules.lua".source = lnk ./lua/rules.lua;
     "hypr/scripts".source = lnk ./scripts;
     "hypr/hyprlock.conf".source = lnk ./hyprlock.conf;
+    "hypr/hyprlock.cybersnake.conf".source = lnk ./hyprlock.cybersnake.conf;
+    "hypr/hyprlock.blacknode.conf".source = lnk ./hyprlock.blacknode.conf;
+    "hypr/hyprlock.berthosefin.conf".source = lnk ./hyprlock.berthosefin.conf;
+    "hypr/hyprlock.hyprcraft.conf".source = lnk ./hyprlock.hyprcraft.conf;
     "hypr/lock-colors.conf".source = lnk ./lock-colors.conf;
     "hypr/lock.jpg".source = lnk "${dotfilesDir}/images/lock/lock.jpg";
     "hypr/lock-scrim.png".source = lnk "${dotfilesDir}/images/lock/lock-scrim.png";
