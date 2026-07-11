100ed46739dfb0878560d21cf2b2fbf97ea4b839
mkiin <mkiin@users.noreply.github.com>
2026-07-12 01:05:17 +0900

    feat(rofi): キャプチャピッカー用の島テーマ capture.rasi を追加

--- home-manager/desktop/rofi/default.nix
+++ home-manager/desktop/rofi/default.nix
@@ -3,6 +3,7 @@
   xdg.configFile = {
     "rofi/config.rasi".source = lnk ./config.rasi;
     "rofi/themes/app-launcher.rasi".source = lnk ./app-launcher.rasi;
+    "rofi/themes/capture.rasi".source = lnk ./capture.rasi;
     "rofi/launch.sh".source = lnk ./launch.sh;
   };
 }
