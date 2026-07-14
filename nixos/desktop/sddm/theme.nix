9ff3efc7a16a392c6c2fa8d5378689a525085904
mkiin <mkiin@users.noreply.github.com>
2026-07-14 15:34:47 +0900

    fix(sddm): star-rail テーマに upstream 削除済みの DIN Next フォントを復元

--- nixos/desktop/sddm/theme.nix
+++ nixos/desktop/sddm/theme.nix
@@ -1,6 +1,7 @@
 {
   stdenvNoCC,
   fetchFromGitHub,
+  fetchurl,
 }:
 stdenvNoCC.mkDerivation {
   pname = "sddm-star-rail-theme";
@@ -13,12 +14,20 @@ stdenvNoCC.mkDerivation {
     hash = "sha256-jVNBiyhdA0lU2CapcgoWO9WlnEF/EBg+JfpPf/G/CzQ=";
   };
 
+  # テーマ本来のフォント。upstream が商用フォントの再配布を避けるため
+  # b6301e7 で削除したが、Main.qml は font/ を読む前提のまま
+  font = fetchurl {
+    url = "https://raw.githubusercontent.com/Darkkal44/qylock/784076fdeb6337134e2c37d0546037569b0ef405/themes/star-rail/font/DINNextW1G-Medium.otf";
+    hash = "sha256-pMhk1Ef6tL7jfzfAWRKMiIAvnpSaZAbHHs/DhjwWRBs=";
+  };
+
   dontBuild = true;
 
   installPhase = ''
     runHook preInstall
     mkdir -p $out/share/sddm/themes
     cp -r themes/star-rail $out/share/sddm/themes/star-rail
+    install -Dm644 $font $out/share/sddm/themes/star-rail/font/DINNextW1G-Medium.otf
     runHook postInstall
   '';
 }
