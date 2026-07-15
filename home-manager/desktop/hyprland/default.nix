ab919563ac9aedb1a333e8a5f7b209cde1ef22d9
mkiin <mkiin@users.noreply.github.com>
2026-07-16 03:04:11 +0900

    refactor(hyprlock): lock.jpg エイリアスを廃止し hyprlang 変数で壁紙ストアを直接参照

--- home-manager/desktop/hyprland/default.nix
+++ home-manager/desktop/hyprland/default.nix
@@ -5,6 +5,9 @@
   dotfilesDir,
   ...
 }:
+let
+  selection = builtins.fromJSON (builtins.readFile "${inputs.self}/images/wallpaper/selection.json");
+in
 {
   imports = [ ./monitor.nix ];
 
@@ -29,8 +32,7 @@
     "hypr/scripts".source = lnk ./scripts;
     "hypr/hyprlock.conf".source = lnk ./hyprlock.conf;
     "hypr/lock-colors.conf".source = lnk ./lock-colors.conf;
-    "hypr/lock.jpg".source =
-      lnk "${dotfilesDir}/images/wallpaper/${(builtins.fromJSON (builtins.readFile "${inputs.self}/images/wallpaper/selection.json")).lock}";
+    "hypr/lock-image.conf".text = "$lockImage = ${dotfilesDir}/images/wallpaper/${selection.lock}";
     "hypr/lock-scrim.png".source = lnk "${dotfilesDir}/images/lock/lock-scrim.png";
   };
 }
