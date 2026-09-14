73c183414ed74aa2959aa259ceb915c2722379f8
mkiin <mkiin@users.noreply.github.com>
2026-09-14 21:53:12 +0900

    feat(game): nixのgameモードの設定を作成

--- modules/nixos/desktop/gaming.nix
+++ modules/nixos/desktop/gaming.nix
@@ -1,1 +1,16 @@
-{ }
+{ config, lib, ... }:
+let
+  cfg = config.modules.desktop.gaming;
+in
+{
+  options.modules.desktop.gaming.enable = lib.mkEnableOption "Install Game Suite";
+
+  config = lib.mkIf cfg.enable {
+    programs.steam = {
+      enable = true;
+      protontricks = true;
+      extest.enable = true;
+    };
+    programs.gamemode.enable = true;
+  };
+}
