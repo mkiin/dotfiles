9fcd5e9e033f36abb6fcaa2e1f9b81ff7eb4caf1
mkiin <mkiin@users.noreply.github.com>
2026-09-14 23:56:35 +0900

    feat(peripheral): 周辺機器の設定

--- modules/nixos/desktop/peripherals.nix
+++ modules/nixos/desktop/peripherals.nix
@@ -1,1 +1,30 @@
-{ }
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    pulseaudio
+  ];
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+  };
+
+  security.rtkit.enable = true;
+  # Disable pulseaudio, it conflicts with pipewire too.
+  services.pulseaudio.enable = false;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;
+
+  services = {
+    printing.enable = true;
+    geoclue2.enable = true;
+  };
+}
