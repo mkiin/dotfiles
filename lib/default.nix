a837bd78b36dc0c8aa7a5f7eab6d294ff311a444
mkiin <mkiin@users.noreply.github.com>
2026-09-14 20:20:28 +0900

    feat(nix): libに同階層のファイルパスを列挙して返す汎用関数を実装

--- lib/default.nix
+++ lib/default.nix
@@ -21,7 +21,6 @@ let
       noto-fonts-cjk-serif = prev.noto-fonts-cjk-serif.override { static = true; };
     })
   ];
-
   mkPkgs =
     system:
     import inputs.nixpkgs {
@@ -54,6 +53,21 @@ let
   };
 in
 {
+  scanPaths =
+    path:
+    builtins.map (f: (path + "/${f}")) (
+      builtins.attrNames (
+        lib.attrsets.filterAttrs (
+          path: _type:
+          (_type == "directory") # include directories
+          || (
+            (path != "default.nix") # ignore default.nix
+            && (lib.strings.hasSuffix ".nix" path) # include .nix files
+          )
+        ) (builtins.readDir path)
+      )
+    );
+
   makeHomeManagerConfig =
     {
       system,
