91bf4483316147a9c3d4145e4c4fd098a4a5ed45
mkiin <blckcaties@gmail.com>
2026-09-20 14:49:31 +0900

    refactor(terminal): ターミナル設定ファアイル移動

--- home/base/desktop/terminal/wezterm.nix
+++ home/base/desktop/terminal/wezterm.nix
@@ -3,9 +3,10 @@
 {
   programs.wezterm = {
     enable = true;
-
-    package = if pkgs.stdenv.hostPlatform.isDarwin then null else pkgs.wezterm;
+    package = pkgs.wezterm;
   };
 
-  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
+  xdg.configFile."wezterm/wezterm.lua" = {
+    source = ./wezterm.lua;
+  };
 }
