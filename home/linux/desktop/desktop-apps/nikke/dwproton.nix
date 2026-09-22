e4626d93fe94919bcf0b3ad41823491afdc31f1e
mkiin <blckcaties@gmail.com>
2026-09-22 17:59:36 +0900

    refactor(nix): フォルダ整理とnikke導入

--- home/linux/desktop/desktop-apps/nikke/dwproton.nix
+++ home/linux/desktop/desktop-apps/nikke/dwproton.nix
@@ -2,13 +2,15 @@
   stdenvNoCC,
   fetchurl,
 }:
-
-stdenvNoCC.mkDerivation rec {
-  pname = "dwproton";
+let
   version = "11.0-13";
+in
+stdenvNoCC.mkDerivation {
+  pname = "dwproton";
+  inherit version;
 
   src = fetchurl {
-    url = "https://git.dawn.wine/dawn-winery/dwproton/releases/download/dwproton-11.0-13/dwproton-11.0-13-x86_64.tar.xz";
+    url = "https://git.dawn.wine/dawn-winery/dwproton/releases/download/dwproton-${version}/dwproton-${version}-x86_64.tar.xz";
     hash = "sha256-lMkSsyBeH5o7lmFOo9w5+jVCETIB4G44LbD3w8XSQfQ=";
   };
 
