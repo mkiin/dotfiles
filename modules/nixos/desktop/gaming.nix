5b8fc70909e28285a5c9c322c7c14cc574c654f7
mkiin <blckcaties@gmail.com>
2026-09-22 12:40:20 +0900

    feat(aagl): sleeyplauncherの追加

--- modules/nixos/desktop/gaming.nix
+++ modules/nixos/desktop/gaming.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
 let
   cfg = config.modules.desktop.gaming;
 in
@@ -6,11 +11,15 @@ in
   options.modules.desktop.gaming.enable = lib.mkEnableOption "Install Game Suite";
 
   config = lib.mkIf cfg.enable {
+    imports = [
+      inputs.aagl.nixosModules.default
+    ];
     programs.steam = {
       enable = true;
       protontricks.enable = true;
       extest.enable = true;
     };
     programs.gamemode.enable = true;
+    programs.sleepy-launcher.enable = true;
   };
 }
