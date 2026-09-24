5962c6eb7cd3e9241a7a2f41e38c5a53c89e05ee
mkiin <blckcaties@gmail.com>
2026-09-25 04:06:34 +0900

    feat(zed): tasks.nixを追加

---
+++ home/base/desktop/zed/tasks.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  programs.zed-editor = {
+    mutableUserTasks = false;
+
+    userTasks = [
+      {
+        label = "lazygit";
+        command = "lazygit";
+        cwd = "$ZED_WORKTREE_ROOT";
+        use_new_terminal = true;
+        allow_concurrent_runs = false;
+      }
+    ];
+  };
+}
